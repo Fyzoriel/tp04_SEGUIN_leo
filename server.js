1e13197c6afd33c9e27dbd5f413ad02e7352ded9
Fyzoriel.le | leo <leoseguin8+github@gmail.com>
2022-10-29 18:56:59 +0200

    fix: rename project

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tp02-seguin-leo'));
+app.use(express.static(__dirname + '/dist/coaster_buy'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/tp02-seguin-leo/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/coaster_buy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
