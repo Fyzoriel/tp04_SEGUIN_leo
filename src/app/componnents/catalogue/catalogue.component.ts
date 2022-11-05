9b174460f4e1b7d9719259001e0f068310051348
Fyzoriel | leo <leoseguin8+github@gmail.com>
2022-11-05 14:16:59 +0100

    fix: remove slider

--- src/app/componnents/catalogue/catalogue.component.ts
+++ src/app/componnents/catalogue/catalogue.component.ts
@@ -27,23 +27,6 @@ export class CatalogueComponent implements OnInit {
   public modelsFilter: string[] = [];
   public nameFilter: string = "";
 
-  minValue: number = 100;
-  maxValue: number = 400;
-  options: Options = {
-    floor: 0,
-    ceil: 500,
-    translate: (value: number, label: LabelType): string => {
-      switch (label) {
-        case LabelType.Low:
-          return `<b>Min price:</b> $${value}`;
-        case LabelType.High:
-          return `<b>Max price:</b> $${value}`;
-        default:
-          return `$${value}`;
-      }
-    }
-  };
-
   public constructor(private readonly productService: ProductService) {}
 
   public ngOnInit(): void {
